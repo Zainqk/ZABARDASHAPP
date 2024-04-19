40c9e9d658073dbcf40433080015a727b4381b0d
@mi477048 <mi477048@gmail.com>
2024-04-19 00:10:54 -0300

    changes added

--- db.ts
+++ db.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 require('dotenv').config();
 const connectDb = async () => {
 	try {
+		// commmen added
 		await mongoose.connect(
 			// `mongodb+srv://user:${process.env.DB_PASSWORD}@cluster0.ybutdar.mongodb.net/`,
 			`mongodb://user:${process.env.DB_PASSWORD}@ac-z7fi0ff-shard-00-00.ybutdar.mongodb.net:27017,ac-z7fi0ff-shard-00-01.ybutdar.mongodb.net:27017,ac-z7fi0ff-shard-00-02.ybutdar.mongodb.net:27017/?replicaSet=atlas-108xga-shard-0&ssl=true&authSource=admin`,
