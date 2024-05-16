ee02475caab18e4e2d3187529bdea02f82daa155
@mi477048 <mi477048@gmail.com>
2024-05-16 15:46:39 -0300

    changes added

--- chat/chatServer copy.ts
+++ chat/chatServer copy.ts
@@ -1,6 +1,9 @@
 import http from 'http';
 import { Server } from 'socket.io';
 
+// Map to store user IDs and their corresponding socket instances
+const userSockets = new Map();
+
 export const setupChatServer = (app: any) => {
 	const server = http.createServer(app);
 	const io = new Server(server, {
@@ -10,31 +13,45 @@ export const setupChatServer = (app: any) => {
 			credentials: true,
 		},
 	});
+
 	io.on('connection', (socket) => {
 		console.log('A user connected');
 
-		// Event listener for message from user
-		// socket.on('message', (message) => {
-		// 	console.log('Received message from client:', message);
+		// Event listener to associate user ID with socket instance
+		socket.on('userId', (userId) => {
+			console.log(`User ${userId} connected`);
+			userSockets.set(userId, socket);
+		});
 
-		// Broadcast the message to all connected user except the sender
-		// socket.broadcast.emit('message', message);
-
-		// Broadcast the message to all connected user
-		// 	io.emit('message', message);
-		// });
-
+		// Event listener for message
 		socket.on('message', (data) => {
 			console.log('Received message from client:', data);
-			const { message, recipientUserId } = data;
+			const { message, senderId, recipientUserIds } = data;
+			const senderSocket = userSockets.get(senderId);
+			senderSocket.emit('message', message);
+			// Iterate over the array of recipient IDs and send the message to each
+			recipientUserIds.forEach((recipientUserId: any) => {
+				const recipientSocket = userSockets.get(recipientUserId);
 
-			// Emit message only to the recipient user
-			io.to(recipientUserId).emit('message', message);
+				if (recipientSocket) {
+					// If recipient is online, send the message only to that socket
+					recipientSocket.emit('message', message);
+				} else {
+					console.log(`Recipient ${recipientUserId} is not online`);
+					// Handle the case where the recipient is not online (optional)
+				}
+			});
 		});
 
 		// Event listener for disconnect
 		socket.on('disconnect', () => {
-			console.log('A user disconnected');
+			// Remove user ID from the map when socket disconnects
+			userSockets.forEach((value, key) => {
+				if (value === socket) {
+					userSockets.delete(key);
+					console.log(`User ${key} disconnected`);
+				}
+			});
 		});
 	});
 
