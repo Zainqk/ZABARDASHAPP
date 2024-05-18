1ff51af011900a374a27d7c956a79fa79fe1eb49
@mi477048 <mi477048@gmail.com>
2024-05-18 00:10:42 -0300

    log added

--- chat/chatServer.ts
+++ chat/chatServer.ts
@@ -16,17 +16,17 @@ export const setupChatServer = (app: any) => {
 	});
 
 	io.on('connection', (socket) => {
-		// console.log('A user connected');
+		console.log('A user connected');
 
 		// Event listener to associate user ID with socket instance
 		socket.on('userId', (userId) => {
-			// console.log(`User ${userId} connected`);
+			console.log(`User ${userId} connected`);
 			userSockets.set(userId, socket);
 		});
 
 		// Event listener for message
 		socket.on('message', async (data) => {
-			// console.log('Received message from client:', data);
+			console.log('Received message from client:', data);
 			const { message, senderId, recipientUserIds, isCustomer } = data;
 			// console.log('The isCustomer value is:', isCustomer);
 			const senderSocket = userSockets.get(senderId);
@@ -76,7 +76,7 @@ export const setupChatServer = (app: any) => {
 					// If recipient is online, send the message only to that socket
 					recipientSocket.emit('message', receiptMessagePayload);
 				} else {
-					// console.log(`Recipient ${recipientUserId} is not online`);
+					console.log(`Recipient ${recipientUserId} is not online`);
 					// Handle the case where the recipient is not online (optional)
 				}
 			});
@@ -88,7 +88,7 @@ export const setupChatServer = (app: any) => {
 			userSockets.forEach((value, key) => {
 				if (value === socket) {
 					userSockets.delete(key);
-					// console.log(`User ${key} disconnected`);
+					console.log(`User ${key} disconnected`);
 				}
 			});
 		});
